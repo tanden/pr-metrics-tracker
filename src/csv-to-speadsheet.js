ea982e079f34f1cc6c4ecacaa17db84534caa880
tanden <sumita.takaki@gmail.com>
2023-06-09 09:50:11 +0900

    modify writeToPullRequestsToSheet

--- src/csv-to-speadsheet.js
+++ src/csv-to-speadsheet.js
@@ -7,3 +7,52 @@ function writeToSheet(csvDataArray, sheetName, destinationSpreadsheetId) {
     sheet.clear();
     sheet.getRange(1, 1, csvDataArray.length, csvDataArray[0].length).setValues(csvDataArray);
 }
+
+function writePullRequestsToSheet(pullRequests, startDate, sheetName, destinationSpreadsheetId) {
+    const pullRequestSheetName = getFormattedDate(new Date(startDate)) + ' / ' + sheetName;
+    const header = [
+        'title',
+        'author',
+        'url',
+        'firstCommittedAt',
+        'PROpenedAt',
+        'firstReviewedAt',
+        'lastApprovedReviewedAt',
+        'mergedAt',
+        'leadTime',
+        'PRLeadTime',
+    ];
+    const csvDataArray = pullRequests.map((pr) => {
+        return [
+            pr.title,
+            pr.author,
+            pr.url,
+            pr.firstCommittedAt,
+            pr.createdAt,
+            pr.firstReviewedAt,
+            pr.lastApprovedReviewedAt,
+            pr.mergedAt,
+            pr.getLeadTime(),
+            pr.getPRLeadTime(),
+        ];
+    });
+    csvDataArray.unshift(header);
+
+    writeToSheet(csvDataArray, pullRequestSheetName, destinationSpreadsheetId);
+}
+
+function getFormattedDate(date) {
+    day = String(date.getDate());
+    if (day.length < 2) {
+        day = '0' + day;
+    }
+
+    month = String(date.getMonth() + 1);
+    if (month.length < 2) {
+        month = '0' + month;
+    }
+
+    year = date.getFullYear();
+
+    return [year, month, day].join('-');
+}
