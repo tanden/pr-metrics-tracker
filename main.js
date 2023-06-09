65ac8ba6894497ca4885b6e77ecb267ed43774ac
tanden <sumita.takaki@gmail.com>
2023-06-09 09:49:22 +0900

    modify output pull request when last interval

--- main.js
+++ main.js
@@ -5,9 +5,14 @@ function main() {
         const summary = [];
         const interval = query.interval;
         const queryFromSheet = query.githubQueryString;
+        let iterationCounter = 0;
         for (let startDate of startDates) {
             const searchQuery = new SearchQuery(startDate, interval, queryFromSheet);
             const pullRequests = fetchPullRequest(searchQuery.getQuery());
+            iterationCounter++;
+            if (iterationCounter === startDates.length) {
+                writePullRequestsToSheet(pullRequests, startDate, query.sheetName, query.destinationSpreadsheetId);
+            }
             const pullRequestMetricsSummary = new PullRequestMetricsSummary(pullRequests, searchQuery.startDate, searchQuery.endDate, searchQuery.interval);
             if (summary.length === 0) {
                 summary.push(pullRequestMetricsSummary.getCsvHeader());
