415f025dae7511662732bddc768c93dbdb63398c
tanden <sumita.takaki@gmail.com>
2023-09-09 23:12:16 +0900

    modify is skip epic flag

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function main() {
         const summary = [];
         const interval = query.interval;
         const queryFromSheet = query.githubQueryString;
+        const isSkipEpic = query.getIsSkipEpic();
         let iterationCounter = 0;
         for (let startDate of startDates) {
             const searchQuery = new SearchQuery(startDate, interval, queryFromSheet);
@@ -13,7 +14,7 @@ function main() {
             if (iterationCounter === startDates.length) {
                 writePullRequestsToSheet(pullRequests, startDate, query.sheetName, query.destinationSpreadsheetId);
             }
-            const pullRequestMetricsSummary = new PullRequestMetricsSummary(pullRequests, searchQuery.startDate, searchQuery.endDate, searchQuery.interval);
+            const pullRequestMetricsSummary = PullRequestMetricsSummaryFactory.create(isSkipEpic, pullRequests, searchQuery);
             const metricsSummaryCsvMapper = new MetricsSummaryCsvMapper(pullRequestMetricsSummary);
             if (summary.length === 0) {
                 summary.push(metricsSummaryCsvMapper.getCsvHeader());
