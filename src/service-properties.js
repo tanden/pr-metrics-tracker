9344400a43c3938c2f89db7d9ce2d61142829dac
tanden <sumita@binc.jp>
2025-03-26 19:57:56 +0900

    get prop

--- src/service-properties.js
+++ src/service-properties.js
@@ -5,3 +5,11 @@ function getQuerySheetId() {
 function getAccessToken() {
     return PropertiesService.getScriptProperties().getProperty("PR_METRICS_TRACKER_PAT");
 }
+
+function getDataSheetId() {
+    return PropertiesService.getScriptProperties().getProperty("PR_METRICS_TRACKER_DATA_SHEET_ID");
+}
+
+function getDashboardSheetId() {
+    return PropertiesService.getScriptProperties().getProperty("PR_METRICS_TRACKER_DASHBOARD_SHEET_ID");
+}
